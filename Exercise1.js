cde7cc654bc94af72bad2b0a6b80b155f89a5ede
Shandy Setiawan <shandysetiawan@Shandys-MacBook-Pro.local>
2020-03-23 15:25:48 +0700

    first commit

---
+++ Exercise1.js
@@ -0,0 +1,53 @@
+/*
+1.
+ALGORITMA--> Menghitung resultan gaya pada sebuah mobil
+-  Mencatat angka massa benda 
+-  Mencatat angka percepatan benda
+-  Resultan gaya sama dengan massa benda dikalikan percepatan benda
+
+PSEUDOCODE -->
+SET "mass of objects" with 600
+SET "acceleration of objects" with 2
+SET "resultant force" without any value
+
+CALCULATE "mass of objects" times "acceleration of objects"
+SET "resultant force" value with calculation result
+
+DISPLAY "resultant force"
+
+2.
+ALGORITMA -->
+
+
+
+PSEUDOCODE -->
+
+
+
+
+3.
+ALGORITMA -->
+
+
+
+PSEUDOCODE -->
+
+SET "clothes" value with 0
+
+IF "clothes" = 40
+    DO "start washing"
+ELSE
+    REPEAT
+    "clothes" INCREMENT 1++
+    UNTIL "clothes" = 40
+ENDIF
+
+
+4.
+ALGORITMA -->
+
+
+
+PSEUDOCODE -->
+
+*/
