c5b2c38944c3b3215e8114ea82d6d242efcc247e
Shandy Setiawan <shandysetiawan@Shandys-MacBook-Pro.local>
2020-03-24 23:29:46 +0700

    continue

---
+++ Exercise2.js
@@ -0,0 +1,20 @@
+// var nama = "" ;
+// var peran = "" ;
+
+// if (nama = "" = true ) {
+//     console.log(`Halo ${nama}, pilih peranmu untuk memulai game!`);
+//         if (peran == "Ksatria") {
+//             console.log(`Selamat datang di Dunia Proxytia, ${nama}
+//             Halo Ksatria +${nama} kamu dapat menyerang dengan senjatamu!`);
+//         } else if (peran == "Tabib") {
+//             console.log(`Selamat datang di Dunia Proxytia, ${nama}
+//             Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
+//         } else if (peran == "Penyihir") {
+//             console.log(`Selamat datang di Dunia Proxytia, ${nama}
+//             Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+//         } else {
+//             console.log(`Pilih peranmu untuk memulai game!`);
+//         }
+// } else {
+//     console.log(`Nama harus diisi!`);
+// }
