8f4be1022955894e8700d56e37a087788ac88507
Shandy Setiawan <shandysetiawan@Shandys-MacBook-Pro.local>
2020-03-26 14:41:08 +0700

    first commit

---
+++ Exercise9.js
@@ -0,0 +1,29 @@
+//1.
+// function shoutOut() {
+//   return "Halo Function!";
+// }
+// console.log(shoutOut());
+
+//2.
+// function calculateMultiply() {
+//   return num1 * num2;
+// }
+
+// var num1 = 5;
+// var num2 = 6;
+
+// var hasilPerkalian = calculateMultiply(num1, num2);
+// console.log(hasilPerkalian);
+
+//3.
+function processSentence() {
+  return `Nama saya ${name}, umur saya ${age}, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`;
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence);
