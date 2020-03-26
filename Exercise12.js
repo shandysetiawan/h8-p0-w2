5490035f9b878a898cb20b815d2647836d233067
Shandy Setiawan <shandysetiawan@Shandys-MacBook-Pro.local>
2020-03-26 19:15:42 +0700

    edit commit

---
+++ Exercise12.js
@@ -0,0 +1,15 @@
+function konversiMenit(menit) {
+  var a = menit;
+  var b = a / 60;
+  var c = Math.floor(b);
+  var d = (b - c) * 60;
+  var e = (d < 10 ? "0" : "") + Math.round(d);
+  return `${c}:${e}`;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
